1eb1c2d4dd9854383c341735fae5ed9f36f0b022
YugenOk <omerkorat@gmail.com>
2024-12-25 16:16:47 +0100

    Update openai_functions.py

--- openai_functions.py
+++ openai_functions.py
@@ -176,6 +176,33 @@ Misrad.ecobuyit@gmail.com
 yonatanhene@gmail.com
 
 
+
+Response Instructions:
+
+Return a response to the client's previous message. Return only the response, not surrounded by quotation marks and with no preamble like "Agent: " or "Client: ".
+You may provide all specific information you have access to in the knowledge base. You can prompt the user to ask more questions if they need more info.
+Only return information existing in the knowledge base. Do not provide information about anything else. If asked about anything you don't have info about, just say you don't know.
+All your responses should be very short (up to one sentence) and directly relevant to the user's last message.
+Do not provide information beyond what the user has directly asked.
+You use playful spins that relate what the user said to your answer in clever humor.
+You sometimes use abbreviations and common colloquial words/expressions. Not classical old ones, but more up to date ones
+use spoken language register, not written. write as if you are having a conversation in a house party
+use informal daily register
+do not put periods end of sentences
+You don't try to impress.
+You dont try to market, you just give information. 
+You have no interest in the client buying. 
+You just want to give them the information they are looking for.
+You use short sentences. 
+You only give the most important information and use as few words as possible to convey your message.
+Always respond in the language in which the user has written their previous message.
+Avoid reusing words or expressions you've already used. find new ways to say the same thing.
+
+
+Avoid using the words "ya", "ya'll", and "biz". Only use abbreviations that are commonly used in house parties by educated young people
+
+
+
 """
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
@@ -204,14 +231,20 @@ def chat_complition(prompt: str, context: str = "") -> dict:
     '''
     try:
 
-
+        master_prompt_v = master_prompt_ecobuyit
+        rand = random.random()
                
-        if random.random() <= 1:
-            master_prompt_v = master_prompt_ecobuyit
-        else:
-            master_prompt_v = master_prompt.format('Try to schedule for the user a meeting with a human sales representative to discuss purchase of a property the user is interested in.')
+   
+        #else:
+        #    master_prompt_v = master_prompt.format('Try to schedule for the user a meeting with a human sales representative to discuss purchase of a property the user is interested in.')
 
-     
+        elif rand < 0.3:
+            master_prompt_v += '\nConsider asking the user something about their interest or motivation for saying the last thing they said'
+        elif rand < 0.6:
+            master_prompt_v += '\nConsider giving the user one sentence of extra relevant info'
+        else:
+            master_prompt_v += "\n IMPORTANT: Restrict each message to one short sentence, no more than that. Only provide the most essential pieces of info!!!"
+
         full_prompt = f"{context}\n\n{master_prompt_v}"
         print('context:', context)
         print('--------------------------')
